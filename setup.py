bd7691da101625dee94b4111cd449f2237cb7d28
yeago <subsume@gmail.com>
2011-10-09 16:14:02 -0400

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-facebook-oauth-tools',
-    version="0.1",
+    version="0.2",
     author='Steve Yeago',
     author_email='subsume@gmail.com',
     description='Django Facebook Oauth Blah Blah Blah',
